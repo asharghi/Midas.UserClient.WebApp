55a0d7136ce8143bb4c1f62a3b6b6213811414b8
asharghi <a.sharghi.mail@gmail.com>
2020-03-28 22:15:54 +0100

    Init

---
+++ ClientApp/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Start msg="Test message"/>
+  </div>
+</template>
+
+<script>
+
+import Start from '@/components/Start.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Start
+  }
+}
+</script>
---
+++ ClientApp/src/views/Store.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="store">
+    <h1>This is the store page</h1>
+  </div>
+</template>
